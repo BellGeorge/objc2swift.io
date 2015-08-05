9543bc4febcfb343e55345755145a738b37da765
John Pope <jp+github@fieldstormapp.com>
2015-08-05 16:28:21 +1000

    v1.1 bump

---
+++ github.rb
@@ -0,0 +1,10 @@
+require 'sinatra'
+require 'rest-client'
+require 'json'
+
+CLIENT_ID = ENV['GH_BASIC_CLIENT_ID']
+CLIENT_SECRET = ENV['GH_BASIC_SECRET_ID']
+
+get '/' do
+  erb :index, :locals => {:client_id => CLIENT_ID}
+end
